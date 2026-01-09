931b9f0d6d0f8649ac7a8716809983e43e88d686
EIprogramming <61566863+EIprogramming@users.noreply.github.com>
2026-01-09 01:18:13 -0500

    Added basic start time and end time selectors
    Added basic start time and end time selectors. TODO: remake in react aria for styling

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -3,14 +3,15 @@ import { RangeCalendar } from './calendar/RangeCalendar';
 import {parseDate, getLocalTimeZone} from '@internationalized/date';
 import {useDateFormatter} from 'react-aria';
 import './Home.css'
+import { getTimeRange } from '../utils.ts'
 
 function Home() {
     const [range, setRange] = useState({
         start: parseDate('2025-02-03'),
         end: parseDate('2025-02-12')
     });
-    const formatter = useDateFormatter({ dateStyle: 'long'});
 
+    const formatter = useDateFormatter({ dateStyle: 'long'});
 
     return (
         <>
@@ -19,15 +20,35 @@ function Home() {
             <input type="text" name="event-title" className="event-title"
             autoComplete="off" placeholder="Event Name"/>
             <span className="event-spacer"></span>
-            <div className="event-calendar-wrapper">
-                <RangeCalendar
-                className="event-calendar react-aria-RangeCalendar"
-                value={range}
-                onChange={setRange} />
-                <p>Selected range: {formatter.formatRange(
-                    range.start.toDate(getLocalTimeZone()),
-                    range.end.toDate(getLocalTimeZone())
-                )}</p>
+            <div className="event-time-wrapper">
+                <div className="event-calendar-wrapper">
+                    <RangeCalendar
+                    className="event-calendar react-aria-RangeCalendar"
+                    value={range}
+                    onChange={setRange} />
+                    <p>Selected range: {formatter.formatRange(
+                        range.start.toDate(getLocalTimeZone()),
+                        range.end.toDate(getLocalTimeZone())
+                    )}</p>
+                </div>
+                <div className="event-start-end-wrapper">
+                    <label htmlFor="event-start-times">Start Time: </label>
+                    {/* TODO: CHANGE SELECTS TO ARIA-REACT SELECTS FOR STYLING */}
+                    <select name="event-start-times">
+                        <option value="" selected disabled hidden>Start Time</option>
+                        {getTimeRange(0, 24).map((time, index) => {
+                            return <option key={`event-start-time${index}`} value={time}>{time}</option>
+                        })}
+                    </select>
+                    <label htmlFor="event-start-times">End Time: </label>
+                    <select name="event-end-times">
+                        <option value="" selected disabled hidden>End Time</option>
+                        {getTimeRange(0, 24).map((time, index) => {
+                            return <option key={`event-end-time${index}`} value={time}>{time}</option>
+                        })}
+                    </select>
+                </div>
+                <button type="submit">Submit!</button>
             </div>
         </form>
         </>
--- src/schedule/Schedule.tsx
+++ src/schedule/Schedule.tsx
@@ -3,23 +3,7 @@ import Timeblock from './Timeblock.tsx'
 import Selector from './Selector.tsx'
 import State from './state.ts';
 import './Schedule.css'
-
-
-/**
- * Convert a 24 hour integer to a formatted 12 hour string 
- * @param hour - the exact integer hour in 24 hour format
- * @returns the time in 12 hour format (e.g. 12:00 pm)
- */
-
-function getHour(hour: number) {
-    let h = hour % 24;
-
-    // if hour is less than 12 it is morning, else it is pm
-    let morningOrAfternoon: string = (h < 12) ? "am" : "pm";
-
-    // return the hour corresponding to the am or pm 
-    return `${(h % 12) ? (h % 12) : 12}:00 ${morningOrAfternoon}`;
-}
+import { getTimeRange } from '../utils.ts'
 
 /**
  * Edits an array in a rectangular section between two corners (firstElement) and (lastElement)
@@ -59,22 +43,6 @@ function editArrayRegion(
  */
 function initialize2DArray(columns: number, rows: number, value: any="") {
     return [...Array(columns)].map(_element => Array(rows).fill(value));
-}
-
-/**
- * Generate a range of times in 12 hour format (e.g. 12:00 pm) between the starting hour (inclusive) and ending hour (inclusive)
- * @param startHour - the beginning hour of the time range, in 24 hour integer form
- * @param endHour - the final hour of the time range, in 24 hour integer form
- * @returns an array of 12 hour formatted times between the starting hour and final hour
- */
-function getTimeRange(startHour: number, endHour: number) {
-    let timeRange: Array<string> = Array(endHour - startHour + 1);
-
-    for (let i = 0; i <= endHour - startHour; i++) {
-        timeRange[i] = getHour(startHour + i);
-    }
-
-    return timeRange;
 }
 
 function Schedule() {
