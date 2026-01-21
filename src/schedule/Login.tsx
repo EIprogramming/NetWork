448e287db3a713d0cc2f986758e4cab2693308d5
EIprogramming <61566863+EIprogramming@users.noreply.github.com>
2026-01-21 00:33:22 -0500

    feat(login): log in basic form validation

--- src/schedule/Login.tsx
+++ src/schedule/Login.tsx
@@ -4,34 +4,47 @@ import './Login.css'
 type User = {
     username: string,
     password: string,
+    confirm: string
 }
 
 function Login() {
-    const { register, handleSubmit, formState: { errors } } = useForm<User>();
+    const { register, handleSubmit, getValues, formState: { errors } } = useForm<User>({mode: 'onChange'});
+    const onSubmit = handleSubmit((data) => console.log(data));
+    const usernameMaxlength = 40;
+    const passwordMaxlength = 40;
+    const passwordMinlength = 15;
+
+    function validateConfirm() {
+        if (getValues("password") !== getValues("confirm")) {
+            return "Passwords must match.";
+        }
+    }
 
     return (
         <div className="schedule-login-overlay">    
             <div className="schedule-login-align">
                 <div className="schedule-login">
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <h2 className="schedule-login-title">Sign Up/Log In</h2>
                         <i className="schedule-login-italic">Please enter a username and optional password.</i>
                         <div className="schedule-login-spacer"></div>
 
-                        <label htmlFor="username">Username</label>
+                        <label htmlFor="username">Name</label>
                         <div></div>
                         <input 
                             id="username" type="text"
                             className="schedule-login-input schedule-login-username"
                             autoComplete="off"
+                            placeholder=""
                             {...register("username", {
-                                required: 'Username is required',
+                                required: "Name is required.",
                                 maxLength: {
-                                    value: 30,
-                                    message: 'Username may not exceed 30 characters.'
+                                    value: usernameMaxlength,
+                                    message: `Name cannot exceed ${usernameMaxlength} characters.`
                                 }
                             })}
                             />
+                        {errors.username && <div role="alert">{errors.username.message}</div>}
 
                         <div className="schedule-login-spacer"></div>
 
@@ -39,10 +52,34 @@ function Login() {
                         <div></div>
                         <input id="password" type="password"
                             className="schedule-login-input schedule-login-password"
-                            placeholder="(optional)" />
+                            placeholder="(optional)" 
+                            {...register("password", {
+                                maxLength: {
+                                    value: passwordMaxlength,
+                                    message: `Password cannot exceed ${passwordMaxlength} characters.`
+                                },
+                                minLength: {
+                                    value: passwordMinlength,
+                                    message: `Password must be more than ${passwordMinlength} characters.`
+                                }
+                            })} />
+                        {errors.password && <div role="alert">{errors.password.message}</div>}
                         
                         <div className="schedule-login-spacer"></div>
+                        
+                        {(getValues("password") || getValues("confirm")) && <>
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <div></div>
+                        <input id="confirm-password" type="password"
+                            className="schedule-login-input schedule-login-password"
+                            placeholder="" 
+                            {...register("confirm", {
+                                validate: validateConfirm,
+                            })} /> 
+                        {errors.confirm && <div role="alert">{errors.confirm.message}</div>}
+                        </>}
 
+                        <div className="schedule-login-spacer"></div>
                         <button type="submit">Sign Up / Log In!</button>
                     </form>
                 </div>
